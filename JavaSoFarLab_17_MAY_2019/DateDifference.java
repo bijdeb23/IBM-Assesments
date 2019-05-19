ec54942abdd512bf19bddeac2dadca3c828f1956
Bijoy Debnath <50030794+bijdeb23@users.noreply.github.com>
2019-05-19 22:43:36 +0530

    Update DateDifference.java

--- JavaSoFarLab_17_MAY_2019/DateDifference.java
+++ JavaSoFarLab_17_MAY_2019/DateDifference.java
@@ -10,21 +10,35 @@ class DateDifference{
         		System.out.print("Enter the date in 'dd/MM/yyyy' format: ");
         		String dateStr = sc.nextLine();
         		StringTokenizer st = new StringTokenizer(dateStr,"/");
-                int[] arr = new int[5];
+                	int[] arr = new int[5];
+                	int day = 0, month = 0, year = 0;
         		int i = 0;
         		while (st.hasMoreTokens()) {
-                    String newStr = st.nextToken();
-                    if(i<3)
-                    {
-        	            arr[i] = Integer.parseInt(newStr);
-        	            i++;
-                	}
-                }
-                Period gap = Period.between(LocalDate.of(arr[2] , arr[1] , arr[0]), LocalDate.now());
+			    String newStr = st.nextToken();
+				if(i<3)
+				{
+				    arr[i] = Integer.parseInt(newStr);
+					if(i == 0)
+					{
+					    day = arr[i];
+					}
+					else if(i == 1)
+					{
+					    month = arr[i];
+					}
+					else if(i == 2)
+					{
+					    year = arr[i];
+					}
 
-                System.out.println("Difference of Days are: " + gap.getDays());
-                System.out.println("Difference of Months are: " + gap.getMonths());
-                System.out.println("Difference of Years are: " + gap.getYears());
+				    i++;
+				}
+			}
+			Period gap = Period.between(LocalDate.of(year , month , day), LocalDate.now());
+
+			System.out.println("Difference of Days are: " + gap.getDays());
+			System.out.println("Difference of Months are: " + gap.getMonths());
+			System.out.println("Difference of Years are: " + gap.getYears());
         }while(true);
 
 	}
